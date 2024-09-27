c0dea2246f339f2202d8d22f1a5ba5f9db7c070e
Tung Nguyen <tungnv@vega.com.vn>
2024-09-27 16:24:42 +0700

    editor: add editor + editor output

--- src/app/(project)/project/[id]/page.tsx
+++ src/app/(project)/project/[id]/page.tsx
@@ -1,19 +1,29 @@
+import Editor from '@/components/editor/editor';
 import AddMembersForm from '@/components/project/addMembersForm';
 import { db } from '@/lib/database';
-import { usersInProject } from '@/lib/database/schema';
-import { eq } from 'drizzle-orm';
+import { notFound } from 'next/navigation';
+// import { usersInProject } from '@/lib/database/schema';
+// import { eq } from 'drizzle-orm';
 
-const getUsersInProject = async (id: string) => {
-  return await db.select().from(usersInProject).where(eq(usersInProject.projectId, id));
+// const getUsersInProject = async (id: string) => {
+//   return await db.select().from(usersInProject).where(eq(usersInProject.projectId, id));
+// };
+
+const getProjectDetail = async (id: string) => {
+  return await db.query.project.findFirst({
+    where: (project, { eq }) => eq(project.id, id),
+  });
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const users = await getUsersInProject(params.id);
+  const project = await getProjectDetail(params.id);
+  console.log({ project });
 
-  console.log({ users });
+  if (!project) return notFound();
 
   return (
     <div className="py-40">
+      <Editor dataHTML={project?.data || ''} title={project?.name || ''} />
       <AddMembersForm />
     </div>
   );
